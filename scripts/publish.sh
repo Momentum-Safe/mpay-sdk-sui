779348eb2b5800d1a9d716b1ca72683985469af4
jacky wang <jackyw.se@gmail.com>
2023-09-25 00:10:11 -0700

    [script] adjusted publish file

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,5 @@
 GIT_COMMIT_SHORT=$(git rev-parse --short HEAD)
 
+cp package.json temp_package.json
 yarn publish --non-interactive --prerelease --preid pre-"${GIT_COMMIT_SHORT}" --no-git-tag-version --access public
+mv temp_package.json package.json
